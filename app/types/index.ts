5376ffa8356e699be83b2d6ee8d8b7b1f23221ed
mirgubad <Mirgubad@MIRGUBAD>
2024-03-06 14:24:42 +0400

    initialized

---
+++ app/types/index.ts
@@ -0,0 +1,24 @@
+import {User, Listing, Reservation} from "@prisma/client"
+
+export type SafeListing = Omit<Listing, "createdAt"> & {
+  createdAt: string
+}
+
+export type SafeUser = Omit<
+  User,
+  "createdAt" | "updatedAt" | "emailVerified"
+> & {
+  createdAt: string
+  updatedAt: string
+  emailVerified: string | null
+}
+
+
+export type SafeReservations = Omit<
+  Reservation,
+  "createdAt" | "startDate"|"endDate" | "listing"> & {
+  createdAt: string;
+  startDate: string;
+  endDate: string;
+  listing: SafeListing;
+}
